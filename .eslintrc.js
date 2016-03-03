beba76a8da7bae194ba1ed4d5d5b2dd71a240a62
fawind <windheuser.fabian@gmail.com>
2016-03-03 19:34:09 +0100

    Add react redux boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['react'],
+  ecmaFeatures: {
+    jsx: true
+  },
+  globals: {
+    React: true
+  },
+  rules: {
+    'no-console': 0,
+  },
+};
